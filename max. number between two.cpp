3566cb81ce2162e2402f9ed8687b212bfa8d92e3
Codspy76 <114976813+Codspy76@users.noreply.github.com>
2022-11-25 19:53:12 +0530

    Add files via upload

---
+++ max. number between two.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter tha number\n");
+	scanf("%d",&a);
+	printf("enter second number");
+	scanf("%d",&b);
+	if(a>b) {
+	
+	printf("greater number is%d", a);
+}	
+else {
+
+
+	printf("greater number is%d=", b);
+	
+}
+	return 0;
+}
